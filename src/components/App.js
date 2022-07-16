e120e6393d93fe755318098074bf836752f738d8
koechronix <koechronix@yahoo.com>
2022-07-16 17:14:04 -0400

    cpompleted fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+
+import React, { useState, useEffect} from "react";
+
+
+function App(){
+    const [dogImg, setDogImg] = useState(null);
+
+    useEffect(()=>{
+        fetch ("https://dog.ceo/api/breeds/image/random")
+        .then((response)=>response.json())
+        .then((data)=>{setDogImg(data.message);
+            // console.log(data);
+        });
+
+    }, []);
+
+    if (!dogImg)
+    return <p>Loading...</p>;
+    else return <img src={dogImg} alt=" A Random Dog"/>;
+
+}
+export default App;
